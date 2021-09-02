398ec1f053f7ddbb0755b8f6352752b16936f615
Subhash <subhashmack@gmail.com>
2021-09-02 16:56:31 +0530

    added env for api

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-
+require('dotenv').config()
+const unsplash_api_key=process.env.REACT_APP_unsplash_api_key
+console.log(unsplash_api_key);
 export default axios.create({
     baseURL:'https://api.unsplash.com',
     headers:{
-        Authorization: 'Client-ID sKUFe7NjveK8Cjk20M1BXREiHcst3ekBSdL1L04wLfE'
+        Authorization: `Client-ID ${unsplash_api_key}`
     }
 });
